c603bb4893941d34a3279e4cc220d62b972b882a
Nour A Alghamdi <65490460+namacoconut@users.noreply.github.com>
2020-10-24 19:52:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# create 2 dects
+
+user1 = {'username':'nour', 'age':'24'}
+user2 = {'username':'sara', 'age':'23'}
+my_users = [user1,user2]
+print(my_users)
+
+for user in my_users:
+  print(user['username'])
